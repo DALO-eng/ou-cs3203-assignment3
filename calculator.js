a8fe025e1c2b1fae6f59c65529c82c68193aedaa
DALO <dalobusiness01@gmail.com>
2023-09-12 22:31:28 -0500

    Add comments'

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
+// Sum an array of numbers
 const sum = function (arr) {
   return arr.reduce((a, b) => a + b, 0);
 };
 
+// Multiply an array of numbers
 const multiply = function (arr) {
   return arr.reduce((a, b) => a * b, 1);
 };
@@ -10,7 +12,9 @@ const reverse = function (arr) {
   return arr.reverse();
 };
 
+// Main function
 const main = function () {
+  // Get the numbers from the command line
   const numbers = process.argv.slice(2).map((x) => +x);
   console.log("Sum: ", sum(numbers));
   console.log("Multiply: ", multiply(numbers));
